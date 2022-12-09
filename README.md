5ffc1f166c2230689cdb1b84326d310fed1a4eb2
Ashok <ashokinstep@gmail.com>
2022-12-09 15:39:18 +0530

    New Website

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# portfolio
+# ashoksingalportfolio.io
+ 
---
+++ port.js
@@ -0,0 +1,106 @@
+const  drop =() => {
+    let cc1 = document.getElementById("drop");
+    cc1.classList.remove('collapse')
+    cc1.classList.add('collapse-show')
+}
+const  dropoff =() => {
+    let cc1 = document.getElementById("drop");
+    cc1.classList.remove('collapse-show')
+    cc1.classList.add('collapse')
+}
+const  opa =() => {
+    let cc1 = document.getElementById("sech2");
+    let cc2 = document.getElementById("secp");
+    cc1.classList.remove('sech2')
+    cc1.classList.add('sech22')
+    cc2.classList.remove('secp')
+    cc2.classList.add('secp2')
+}
+const  loader =() => {
+    let cc1 = document.getElementById("loader");
+   
+    cc1.classList.add('d-none')
+}
+const  preview =() => {
+    let cc1 = document.getElementById("pre");
+    cc1.classList.remove('d-none')
+ 
+}
+const  previewoff =() => {
+    let cc1 = document.getElementById("pre");
+    cc1.classList.add('d-none')
+ 
+}
+
+const  preview2 =() => {
+    let cc1 = document.getElementById("pre-2");
+    
+   
+    cc1.classList.remove('d-none')
+    
+}
+const  previewoff2 =() => {
+    let cc1 = document.getElementById("pre-2");
+    
+   
+    cc1.classList.add('d-none')
+    
+}
+const  preview3 =() => {
+    let cc1 = document.getElementById("pre-3");
+    
+   
+    cc1.classList.remove('d-none')
+    
+}
+const  previewoff3 =() => {
+    let cc1 = document.getElementById("pre-3");
+    
+   
+    cc1.classList.add('d-none')
+    
+}
+const  preview4 =() => {
+    let cc1 = document.getElementById("pre-4");
+    
+   
+    cc1.classList.remove('d-none')
+    
+}
+const  previewoff4=() => {
+    let cc1 = document.getElementById("pre-4");
+    
+   
+    cc1.classList.add('d-none')
+    
+}
+const  preview5 =() => {
+    let cc1 = document.getElementById("pre-5");
+    
+   
+    cc1.classList.remove('d-none')
+    
+}
+const  previewoff5 =() => {
+    let cc1 = document.getElementById("pre-5");
+    
+   
+    cc1.classList.add('d-none')
+    
+}
+const  preview6 =() => {
+    let cc1 = document.getElementById("pre-6");
+    
+   
+    cc1.classList.remove('d-none')
+    
+}
+const  previewoff6 =() => {
+    let cc1 = document.getElementById("pre-6");
+    
+   
+    cc1.classList.add('d-none')
+    
+}
+
+    
